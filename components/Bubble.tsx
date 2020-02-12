3a30af72316bd3209c165054be205ded6b73e0d9
Jared Walters <jared.w.walters@gmail.com>
2020-02-11 16:43:34 -0800

    Create bubble sort component, add, manual step through for visual testing, add simple stylings

--- components/Bubble.tsx
+++ components/Bubble.tsx
@@ -1,36 +1,87 @@
 import React, { useState } from 'react';
+import { log } from 'util';
 
 const Bubble = (props: { items: number[] }) => {
     const { items } = props;
     const [changed, setChanged] = useState(false);
     const [current, setCurrent] = useState(0);
+    const [inner, setInner] = useState(0);
     const [next, setNext] = useState(current + 1);
 
-    
+    /**
+     * Start at step 0
+     * Clicking button will move to next step
+     */
+    if (current < items.length - 1) {
+        if (inner < items.length - (current + 1)) {
+            // Reset changed value with each iteration
+            if (changed) {
+                setChanged(false)
+            }
 
-    for (let i = 0; i < items.length - 1; i++) {
-        // If the array is not need changed in a given pass then it is already sorted
-        let changed = false;
-        // Because the largest number will bubble to the highest index of the array in each iteration,
-        // use `j < items.length - (i + 1)` to avoid sorting the already sorted parts at the end of the array
-        for (let j = 0; j < items.length - (i + 1); j++) {
-            if (items[j] > items[j + 1]) {
+            if (items[inner] > items[inner + 1]) {
+                console.log(`First: ${items[inner]} Second: ${items[inner + 1]}`);
                 // use array destructuring to avoid temp variable
-                [items[j], items[j + 1]] = [items[j + 1], items[j]];
+                [items[inner], items[inner + 1]] = [items[inner + 1], items[inner]];
 
                 // A change was made this pass, so the array was not sorted prior to this pass
                 // changed = true;
                 setChanged(true);
+                console.log(`current: ${current}, inner: ${inner}`);
             }
         }
 
-        if (!changed) {
-            break;
-        }
+        // if (inner === items.length - (current + 1)) {
+        //     console.log('increment current');
+        //     setCurrent(current + 1);
+        // }
+
+        console.log(`Changed: ${changed}`);
     }
 
+    const tileStyle = (index: number) => {
+        if (index === inner) {
+            return { color: 'green' };
+        }
+
+        if (index === inner + 1) {
+            return { color: 'blue' };
+        }
+
+        return { color: 'red' };
+    };
+
+    const tiles = () => {
+        return items.map((item, index) => {
+            const style = {
+                ...tileStyle(index),
+                width: '40px',
+                height: '40px',
+                border: '1px solid red',
+            };
+            return (
+                <div style={style} key={item}>
+                    {item}
+                    <div>{index}</div>
+                </div>
+            )
+        });
+    };
+
     return (
-        <div>{items}</div>
+        <>
+            <button onClick={() => {
+                console.log('test');
+                // When inner counter reaches number of items then reset it, otherwise increment
+                inner < (items.length - 1) ? setInner(inner + 1) : setInner(0);
+            }}>
+                Step
+            </button>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+                {tiles()}
+            </div>
+            {!changed && <div>sorted!</div>}
+        </>
     )
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { NextPage } from 'next';
 import { Bubble } from '../components/Bubble';
 
 const Home: NextPage<{ userAgent: string }> = () => (
-    <Bubble items={[5,2,1,3,4]} />
+    <Bubble items={[5,8,1,3,4,6,7]} />
 );
 
 Home.getInitialProps = async ({ req }) => {
